667d03d7a2826df57e19a91c442fbd2dcca122a7
jwoehr <jwoehr@205c91b2-b897-4973-8c04-09dba22130b7>
2015-11-16 18:11:05 +0000

    slight cleanup

--- com/SoftWoehr/FIJI/FIJIGui.java
+++ com/SoftWoehr/FIJI/FIJIGui.java
@@ -11,133 +11,163 @@
  *
  * Created on October 15, 2000, 7:49 PM
  */
-
 package com.SoftWoehr.FIJI;
+
 import com.SoftWoehr.util.GetArgs;
 import com.SoftWoehr.util.Argument;
 import com.SoftWoehr.JaXWT.StockDialog;
 
-/** The application controller for the GUI form of FIJI.
+/**
+ * The application controller for the GUI form of FIJI.
  *
  * @author jax
- * @version $Id: FIJIGui.java,v 1.3 2008-10-20 18:04:36 jwoehr Exp $
+ * @version $Id: FIJIGui.java,v 1.4 2015-11-16 18:11:05 jwoehr Exp $
  */
 public class FIJIGui extends Object implements Runnable {
-    
-    /** The screen title */
+
+    /**
+     * The screen title
+     */
     public static final String BASE_TITLE = "SoftWoehr FIJI ForthIsh Java Interpreter";
     public static final String RELEASE_LEVEL = "gamma";
-    
-    private FijiProperties properties = new FijiProperties();
-    
+
+    private final FijiProperties properties = new FijiProperties();
+
     private String props_file_name;
-    
-    /** If true allow System.exit(); */
+
+    /**
+     * If true allow System.exit();
+     */
     private boolean systemExiting = true;
-    
-    /** Creates new FIJIGui */
+
+    /**
+     * Creates new FIJIGui
+     */
     public FIJIGui() {
     }
-    
-    /** True if it's okay to System.exit() when closing the last FIJIFrame.
-     * @return <CODE>true</CODE> means that it's okay to call System.exit() on closing the last FIJIFrame.
+
+    /**
+     * True if it's okay to System.exit() when closing the last FIJIFrame.
+     *
+     * @return <CODE>true</CODE> means that it's okay to call System.exit() on
+     * closing the last FIJIFrame.
      */
     public boolean isSystemExiting() {
         return systemExiting;
     }
-    
-    /** Set true, a close of the session will invoke System.exit() when the last FIJIFrame closes.
-     * @param yesNo If <CODE>false</CODE> then closing the last FIJIFrame will not explicitly call System.exit(0);
+
+    /**
+     * Set true, a close of the session will invoke System.exit() when the last
+     * FIJIFrame closes.
+     *
+     * @param yesNo If <CODE>false</CODE> then closing the last FIJIFrame will
+     * not explicitly call System.exit(0);
      */
     public void setSystemExiting(boolean yesNo) {
         systemExiting = yesNo;
     }
-    
-    /** Return an application property from the app controller.
+
+    /**
+     * Return an application property from the app controller.
+     *
      * @param key String indicating sought property.
      * @return String representing sought property.
      */
     public String getProperty(String key) {
         return properties.getProperty(key);
     }
-    
-    /** True if the it's okay with this instance to exit the application.
+
+    /**
+     * True if the it's okay with this instance to exit the application.
+     *
      * @param f The associated FIJI Frame requesting permission to exit.
      * @return <CODE>true</CODE> if the frame may exit.
      */
-    protected boolean exit_requested (FijiFrame f) {
+    protected boolean exit_requested(FijiFrame f) {
         boolean result = true;
         // ...
         return result;
     }
-    
-    /** True if the it's okay with this instance to exit the application.
+
+    /**
+     * True if the it's okay with this instance to exit the application.
+     *
      * @param f The associated FIJI Frame requesting permission to exit.
      * @return <CODE>true</CODE> if the frame may exit.
      */
-    protected boolean system_exit_requested (FijiFrame f) {
+    protected boolean system_exit_requested(FijiFrame f) {
         // /* Debug */ System.out.println(isSystemExiting());
         return isSystemExiting();
     }
-    
-    /** Create a new fiji gui frame with this as app controller
+
+    /**
+     * Create a new fiji gui frame with this as app controller
+     *
      * @return the new FijiFrame
      */
     protected FijiFrame new_frame() {
         return new FijiFrame(this);
     }
-    
-    /** Set name of last loaded or saved props file.
-     * @return  The name of the saved or loaded props file. */
+
+    /**
+     * Set name of last loaded or saved props file.
+     *
+     * @return The name of the saved or loaded props file.
+     */
     protected String get_props_file_name() {
         return props_file_name;
     }
-    
-    /** Get name of last loaded or saved props file.
+
+    /**
+     * Get name of last loaded or saved props file.
+     *
      * @param name Fully-qualified or relative name of props file to load
      */
     protected void set_props_file_name(String name) {
         props_file_name = name;
     }
-    
-    /** Just a little error reporter */
+
+    /**
+     * Just a little error reporter
+     */
     private void exception_loading_props_file(Exception e) {
         e.printStackTrace(System.err);
         StockDialog.showExceptionDialog(null, "Couldn't load properties file " + props_file_name + " .", e, "Exception loading properties file");
     }
-    
-    /** Load whatever the props file name is as properties file
-     * @return <CODE>true</CODE> if file loaded okay, <CODE>false</CODE> otherwise.
+
+    /**
+     * Load whatever the props file name is as properties file
+     *
+     * @return <CODE>true</CODE> if file loaded okay, <CODE>false</CODE>
+     * otherwise.
      */
-    protected boolean load_props () {
+    protected boolean load_props() {
         boolean result = false;
-        if (null != props_file_name){
+        if (null != props_file_name) {
             try {
                 properties.load_from_file(props_file_name);
                 result = true;
-            }
-            catch (java.io.FileNotFoundException e) {
+            } catch (java.io.FileNotFoundException e) {
+                exception_loading_props_file(e);
+            } catch (java.io.IOException e) {
                 exception_loading_props_file(e);
             }
-            catch (java.io.IOException e) {
-                exception_loading_props_file(e);
-            }
-        }
-        else {
+        } else {
             StockDialog.showErrorDialog(null, "No properties file name assigned.", "Error attempting to load properties file");
         }
         return result;
     }
-    
-    /** Process command line options */
+
+    /**
+     * Process command line options
+     */
     private void process_options(String args[]) {
         int i;
         Argument a;
-        
+
         GetArgs g = new GetArgs(args);
-        for (i = 0; i < g.optionCount() ; i++)
-        {
-            a =  g.nthOption(i);
+        for (i = 0; i < g.optionCount(); i++) {
+            a = g.nthOption(i);
             if (null != a.option) {
                 if (a.option.equals("-p")) {
                     if (null != a.argument) {
@@ -148,25 +178,28 @@ public class FIJIGui extends Object implements Runnable {
             }
         }
     }
-    
-    /** Run the GUI version of FIJI.
+
+    /**
+     * Run the GUI version of FIJI.
+     *
      * @param args the command line arguments
      */
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         FIJIGui fiji = new FIJIGui();
         fiji.process_options(args);
         FijiFrame frame = fiji.new_frame();
         frame.setOutToFijiTextArea();
         frame.setErrToFijiTextArea();
-	frame.show ();
+        frame.show();
     }
-    
-    /** Run when used to create a Thread.
-     * In this state, exiting the frame doesn't call System.exit().
+
+    /**
+     * Run when used to create a Thread. In this state, exiting the frame
+     * doesn't call System.exit().
      */
     public void run() {
         setSystemExiting(false);
-        new_frame().show ();
+        new_frame().show();
     }
-    
+
 }
