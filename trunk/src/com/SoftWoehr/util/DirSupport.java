7e331bd8c2d3442304234a7560d3562e6f443826
jwoehr <jwoehr@205c91b2-b897-4973-8c04-09dba22130b7>
2016-11-08 01:10:54 +0000

    cleanup and modernize

--- trunk/src/com/SoftWoehr/util/DirSupport.java
+++ trunk/src/com/SoftWoehr/util/DirSupport.java
@@ -1,104 +1,129 @@
-/*********************************************/
+/** ****************************************** */
 /* DirSupport.java                           */
-/* jack j. woehr jax@well.com jwoehr@ibm.net */
-/* http://www.well.com/user/jax/rcfb         */
-/* P.O. Box 51, Golden, Colorado 80402-0051  */
-/*********************************************/
-/*   Copyright *C* 1998, 2001 Jack J. Woehr  */
-/*	      All Rights Reserved	     */
-/* PO Box 51 Golden, Colorado 80402-0051 USA */
-/*	    http://www.softwoehr.com	     */
-/*	  http://fiji.sourceforge.net	     */
-/*                                           */
-/*       This Program is Free                */
-/*            Softwoehr                      */
-/*                                           */
-/* Permission to distribute this Softwoehr   */
-/* with copyright notice attached is granted.*/
-/*                                           */
-/* Permission to modify for personal use at  */
-/* at home or for your personal use on the   */
-/* job is granted, but you may not publicly  */
-/* make available modified versions of this  */
-/* program without asking and getting the    */
-/* permission of the author, Jack Woehr.     */
-/*                                           */
-/* The permission will usually be granted if */
-/* granted reciprocally by you for the mods. */
-/*                                           */
-/* THERE IS NO GUARANTEE, NO WARRANTY AT ALL */
-/*********************************************/
+ /* jack j. woehr jax@well.com jwoehr@ibm.net */
+ /* http://www.well.com/user/jax/rcfb         */
+ /* P.O. Box 51, Golden, Colorado 80402-0051  */
+/** ****************************************** */
+/*   Copyright *C* 1998, 2016 Jack J. Woehr  */
+ /*	      All Rights Reserved	     */
+ /* PO Box 51 Golden, Colorado 80402-0051 USA */
+ /*	    http://www.softwoehr.com	     */
+ /*	  http://fiji.sourceforge.net	     */
+ /*                                           */
+ /*       This Program is Free                */
+ /*            Softwoehr                      */
+ /*                                           */
+ /* Permission to distribute this Softwoehr   */
+ /* with copyright notice attached is granted.*/
+ /*                                           */
+ /* Permission to modify for personal use at  */
+ /* at home or for your personal use on the   */
+ /* job is granted, but you may not publicly  */
+ /* make available modified versions of this  */
+ /* program without asking and getting the    */
+ /* permission of the author, Jack Woehr.     */
+ /*                                           */
+ /* The permission will usually be granted if */
+ /* granted reciprocally by you for the mods. */
+ /*                                           */
+ /* THERE IS NO GUARANTEE, NO WARRANTY AT ALL */
+/** ****************************************** */
 /* @author  $Author: jwoehr $                         */
-/* @version $Revision: 1.1 $                       */
-
+ /* @version $Revision: 1.2 $                       */
 package com.SoftWoehr.util;
+
 import java.io.*;
 
 /**
  * A class offering support for working with directories.
- * @author  $Author: jwoehr $
- * @version $Revision: 1.1 $
+ *
+ * @author $Author: jwoehr $
+ * @version $Revision: 1.2 $
  */
 public class DirSupport extends Object {
-    
-    /** Revision level */
-    private static final String rcsid = "$Id: DirSupport.java,v 1.1 2016-11-06 21:20:42 jwoehr Exp $";
-    
-    /** Implements com.SoftWoehr.SoftWoehr
+
+    /**
+     * Revision level
+     */
+    private static final String rcsid = "$Id: DirSupport.java,v 1.2 2016-11-08 01:05:47 jwoehr Exp $";
+
+    /**
+     * Implements com.SoftWoehr.SoftWoehr
+     *
      * @return The RCS string.
      */
-    public String rcsId() {return rcsid;}
-    
+    public String rcsId() {
+        return rcsid;
+    }
+
     private File dir;
-    private String [] entries;
-    private File [] files;
-    
-    /** Open a <code>DirSupport</code> on the current directory.
+    private String[] entries;
+    private File[] files;
+
+    /**
+     * Open a <code>DirSupport</code> on the current directory.
      */
     public DirSupport() {
         this(".");
     }
-    
-    /** Open a <code>DirSupport</code> on the named directory.
+
+    /**
+     * Open a <code>DirSupport</code> on the named directory.
+     *
      * @param dirName Name of directory to open on.
      */
     public DirSupport(String dirName) {
         instance(dirName);
     }
-    
-    /** shutdown() here does nothing.
+
+    /**
+     * shutdown() here does nothing.
+     *
      * @see com.SoftWoehr.SoftWoehr#shutdown
-     * @return Zero (0) if no problem 
+     * @return Zero (0) if no problem
      */
-    public int shutdown() { return 0; }
-    
-    /** Cleans up our probable large memory usage as we exit
+    public int shutdown() {
+        return 0;
+    }
+
+    /**
+     * Cleans up our probable large memory usage as we exit
+     *
      * @throws Throwable If an error occurs.
      */
-    protected void finalize() throws Throwable { /* Called by garbage collector in case no longer referenced*/
+    @Override
+    protected void finalize() throws Throwable {
+        /* Called by garbage collector in case no longer referenced*/
         super.finalize();
-        dir = null; System.gc();         /* Make sure any dir lock is released.*/
+        dir = null;
+        System.gc();
+        /* Make sure any dir lock is released.*/
     }
-    
-    /** Instance or re-instance the <code>DirSupport</code> on a named dir.
-     * Discards previous instancing and all related data. Dir locks are
-     * freed by calling <code>System.gc()</code>
+
+    /**
+     * Instance or re-instance the <code>DirSupport</code> on a named dir.
+     * Discards previous instancing and all related data. Dir locks are freed by
+     * calling <code>System.gc()</code>
+     *
      * @param dirName Directory to open on.
      */
-    public void instance(String dirName) {
-        dir     = new File(dirName); System.gc();
-        files   = null;
+    public final void instance(String dirName) {
+        dir = new File(dirName);
+        System.gc();
+        files = null;
         entries = null;
         refresh();
     }
-    
-    /** Refresh the list of entries.
+
+    /**
+     * Refresh the list of entries.
      */
     public void refresh() {
         entries = dir.list();
     }
-    
-    /** Creates a <code>java.io.File</code> for each entry.
+
+    /**
+     * Creates a <code>java.io.File</code> for each entry.
      */
     public void thicken() {
         files = new File[entries.length];
@@ -110,66 +135,78 @@ public class DirSupport extends Object {
         }
         for (int i = 0; i < entries.length; i++) {
             files[i] = new File(path + entries[i]);
-        }                                                          /* End for*/
+        }
+        /* End for*/
     }
-    
-    /** Prints all entries.
+
+    /**
+     * Prints all entries.
      */
     public void printEntries() {
-        for (int i = 0; i < entries.length; i++) {
-            System.out.println(entries[i]);
-        }                                                          /* End for*/
+        for (String entrie : entries) {
+            System.out.println(entrie);
+        }
+        /* End for*/
     }
-    
-    /** Prints all files. This is the thicken information.
+
+    /**
+     * Prints all files. This is the thicken information.
      */
     public void printFList() {
-        String s [] = getFList();
-        for (int i = 0; i < s.length; i++) {
-            System.out.println(s[i]);
-        }                                                          /* End for*/
+        String s[] = getFList();
+        for (String item : s) {
+            System.out.println(item);
+        }
+        /* End for*/
     }
-    
-    /** Get the name of the directory referenced by this instance.
+
+    /**
+     * Get the name of the directory referenced by this instance.
+     *
      * @return Name of directory.
      */
     public String getName() {
         return dir.getName();
     }
-    
-    /** Get a string representing all the thickened file info.
+
+    /**
+     * Get a string representing all the thickened file info.
+     *
      * @return The thickened file info
      */
-    public String [] getFList() {
+    public String[] getFList() {
         thicken();
-        String flist [] = new String [entries.length];
+        String flist[] = new String[entries.length];
         for (int i = 0; i < flist.length; i++) {
             String s = entries[i];
             File f = files[i];
             if (f.isFile()) {
-                s = s + " " + new Long(f.length()).toString();
-            }
-            else {
+                s = s + " " + Long.toString(f.length());
+            } else {
                 if (f.isDirectory()) {
                     s = s + " <DIR>";
+                } else {
+                    s = s + " <???>";
                 }
-                else {
-                    s = s+ " <???>";
-                }                                                       /* End if*/
-            }                                                         /* End if*/
+                /* End if*/
+            }
+            /* End if*/
             flist[i] = s;
-        }                                                          /* End for*/
+        }
+        /* End for*/
         return flist;
     }
-    
-    /** Demonstrate <code>DirSupport</code>.
+
+    /**
+     * Demonstrate <code>DirSupport</code>.
+     *
      * @param argv The dir to open on.
      */
     public static void main(String argv[]) {
         DirSupport d;
-        
-        for (int i = 0; i < argv.length; i++) {
-            d = new DirSupport(argv[i]);
+
+        for (String argv1 : argv) {
+            d = new DirSupport(argv1);
             System.out.println("Class:         " + d.getClass().getName());
             System.out.println("Revision info: " + d.rcsId());
             System.out.print("Slim Directory of ");
@@ -181,7 +218,8 @@ public class DirSupport extends Object {
             System.out.print(d.getName());
             System.out.println(" ...");
             d.printFList();
-        }                                                          /* End for*/
+        }
+        /* End for*/
     }
 }
 /* End of DirSupport.java */
